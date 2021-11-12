b755fc98e19028e7055aa8d7ee3da5ae3aacb4bf
Anta <j0siddy@gmail.com>
2021-11-11 21:20:00 -0600

    Add TwoSum solution

---
+++ src/Java/TwoSum/Runner.java
@@ -0,0 +1,12 @@
+package Java.TwoSum;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] input = new int[]{3,2,4};
+
+        System.out.println(Arrays.toString(twoSum.twoSum(input, 6)));
+    }
+}
